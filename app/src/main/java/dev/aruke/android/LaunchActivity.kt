174b6850df5102bf86b079f6424aa308928f8df2
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-06-05 20:02:46 +0900

    :wrench: Added launch activity

---
+++ app/src/main/java/dev/aruke/android/LaunchActivity.kt
@@ -0,0 +1,14 @@
+package dev.aruke.android
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
