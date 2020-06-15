cc935a94eeb7a5dc96f4a9a5524442308bbbe257
Rajanikant Deshmukh <rajanikant.deshmukh@gmail.com>
2020-06-15 23:22:58 +0900

    :wrench: Configure Epoxy auto model generation

---
+++ app/src/main/java/dev/aruke/android/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This file tells Epoxy to generate the data binding models for specified layout files.
+ */
+@EpoxyDataBindingLayouts({})
+package dev.aruke.android;
+
+import com.airbnb.epoxy.EpoxyDataBindingLayouts;
+
